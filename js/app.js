4ec64437c7f7be2b1ae5d6306c8f787cfe0451f7
adriansikhsan <92657701+ikhsanadrians@users.noreply.github.com>
2022-05-29 22:56:22 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+$('.scrollbar').on('click',function(e){ 
+
+var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ });
+
+ e.preventDefault();
+});
