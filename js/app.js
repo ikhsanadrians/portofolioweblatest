e56a6a0723c480d79e3a12480050d2633c770346
adriansikhsan <92657701+ikhsanadrians@users.noreply.github.com>
2022-06-25 18:23:28 +0700

    Fix ScrollPos

--- js/app.js
+++ js/app.js
@@ -1,29 +1,5 @@
-
-
-$('.scrollbar').on('click',function(e){ 
-
-var tujuan = $(this).attr('href');
-
- var elemenTujuan = $(tujuan);
-
- $('html , body').animate({
-  scrollTop: elemenTujuan.offset().top - 50
- });
-
- e.preventDefault();
-});
-
-
-let darkmode = document.getElementsByClassName("modemalam")[0];
-let lightmode = document.getElementsByClassName("modesiang")[0];
-
-lightmode.addEventListener("click",function(){
-      lightmode.classList.toggle("sembunyikan");
-      if(lightmode.classList.containts("sembunyikan")){
-        darkmode.classList.remove("sembunyikan");
-    }
-});
-
+let scrollpos = window.scrollY;
+let header = document.getElementById("nav");
 
 
 function addClassOnScroll() {
